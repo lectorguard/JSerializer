160325f6b626dee392d0574cfcde6346c34b906b
Simon <keller.simon1998@web.de>
2022-10-21 19:34:03 +0200

    added naming convention for helper macros

--- JSerializerLib/Include/Utils/Macros.h
+++ JSerializerLib/Include/Utils/Macros.h
@@ -1,56 +1,56 @@
 #pragma once
 #include <variant>
 
-#define EXPAND( x ) x
-#define F(x, ...) X = x and VA_ARGS = __VA_ARGS__
-#define G(...) EXPAND( F(__VA_ARGS__) )
+#define JSER_HELPER_EXPAND( x ) x
+#define JSER_HELPER_F(x, ...) X = x and VA_ARGS = __VA_ARGS__
+#define JSER_HELPER_G(...) JSER_HELPER_EXPAND( JSER_HELPER_F(__VA_ARGS__) )
 
 
 // Make a FOREACH macro
-#define FE_0(WHAT)
-#define FE_1(WHAT, X) WHAT(X)
-#define FE_2(WHAT, X, ...) WHAT(X)EXPAND(FE_1(WHAT, __VA_ARGS__))
-#define FE_3(WHAT, X, ...) WHAT(X)EXPAND(FE_2(WHAT, __VA_ARGS__))
-#define FE_4(WHAT, X, ...) WHAT(X)EXPAND(FE_3(WHAT, __VA_ARGS__))
-#define FE_5(WHAT, X, ...) WHAT(X)EXPAND(FE_4(WHAT, __VA_ARGS__))
-#define FE_6(WHAT, X, ...) WHAT(X)EXPAND(FE_5(WHAT, __VA_ARGS__))
-#define FE_7(WHAT, X, ...) WHAT(X)EXPAND(FE_6(WHAT, __VA_ARGS__))
-#define FE_8(WHAT, X, ...) WHAT(X)EXPAND(FE_7(WHAT, __VA_ARGS__))
-#define FE_9(WHAT, X, ...) WHAT(X)EXPAND(FE_8(WHAT, __VA_ARGS__))
-#define FE_10(WHAT, X, ...) WHAT(X)EXPAND(FE_9(WHAT, __VA_ARGS__))
-#define FE_11(WHAT, X, ...) WHAT(X)EXPAND(FE_10(WHAT, __VA_ARGS__))
-#define FE_12(WHAT, X, ...) WHAT(X)EXPAND(FE_11(WHAT, __VA_ARGS__))
-#define FE_13(WHAT, X, ...) WHAT(X)EXPAND(FE_12(WHAT, __VA_ARGS__))
-#define FE_14(WHAT, X, ...) WHAT(X)EXPAND(FE_13(WHAT, __VA_ARGS__))
-#define FE_15(WHAT, X, ...) WHAT(X)EXPAND(FE_14(WHAT, __VA_ARGS__))
-#define FE_16(WHAT, X, ...) WHAT(X)EXPAND(FE_15(WHAT, __VA_ARGS__))
-#define FE_17(WHAT, X, ...) WHAT(X)EXPAND(FE_16(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_0(WHAT)
+#define JSER_HELPER_FE_1(WHAT, X) WHAT(X)
+#define JSER_HELPER_FE_2(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_1(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_3(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_2(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_4(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_3(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_5(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_4(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_6(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_5(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_7(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_6(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_8(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_7(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_9(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_8(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_10(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_9(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_11(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_10(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_12(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_11(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_13(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_12(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_14(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_13(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_15(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_14(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_16(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_15(WHAT, __VA_ARGS__))
+#define JSER_HELPER_FE_17(WHAT, X, ...) WHAT(X)JSER_HELPER_EXPAND(JSER_HELPER_FE_16(WHAT, __VA_ARGS__))
 //... repeat as needed
 
-#define GET_MACRO(_0,_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14,_15,_16,_17,NAME,...) NAME 
-#define FOR_EACH(action,...) \
-    EXPAND(GET_MACRO(_0,__VA_ARGS__,FE_17,FE_16,FE_15,FE_14,FE_13,FE_12,FE_11,FE_10,FE_9,FE_8,FE_7,FE_6,FE_5,FE_4,FE_3,FE_2,FE_1,FE_0,)(action,__VA_ARGS__))
+#define JSER_HELPER_GET_MACRO(_0,_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14,_15,_16,_17,NAME,...) NAME 
+#define JSER_HELPER_FOR_EACH(action,...) \
+   JSER_HELPER_EXPAND(JSER_HELPER_GET_MACRO(_0,__VA_ARGS__,JSER_HELPER_FE_17,JSER_HELPER_FE_16,JSER_HELPER_FE_15,JSER_HELPER_FE_14,JSER_HELPER_FE_13, \
+                        JSER_HELPER_FE_12,JSER_HELPER_FE_11,JSER_HELPER_FE_10,JSER_HELPER_FE_9,JSER_HELPER_FE_8,JSER_HELPER_FE_7,JSER_HELPER_FE_6,JSER_HELPER_FE_5, \
+                        JSER_HELPER_FE_4,JSER_HELPER_FE_3,JSER_HELPER_FE_2,JSER_HELPER_FE_1, JSER_HELPER_FE_0,)(action,__VA_ARGS__))
 
 // Actions
-#define COMMA ,
-#define STRWITHCOMMA(X) #X COMMA
-#define COMMACONTENT(X) COMMA X
-#define OPEN_BRACKET  {
-#define CLOSE_BRACKET }
+#define JSER_HELPER_COMMA ,
+#define JSER_HELPER_STRWITHCOMMA(X) #X JSER_HELPER_COMMA
+#define JSER_HELPER_COMMACONTENT(X) JSER_HELPER_COMMA X
+#define JSER_HELPER_OPEN_BRACKET  {
+#define JSER_HELPER_CLOSE_BRACKET }
 
 // Create JSER Manager Type for Serialization
-#define DEFAUL_SERIALIZATION_CONSTRUCTOR_HELPER jser::ListVectorDequeSerializer ,jser::PolymorphicSerializer, \
-                                                jser::SetSerializer,jser::ArraySerializer,jser::ValarraySerializer, \
-                                                jser::ForwardListSerializer,jser::MapSerializer,jser::TupleSerializer, \
-                                                jser::BitsetSerializer,jser::StackAndQueueSerializer,jser::PriorityQueueSerializer,\
-                                                jser::PointerSerializer
+#define JSER_HELPER_DEFAUL_SERIALIZATION_CONSTRUCTOR_HELPER jser::ListVectorDequeSerializer ,jser::PolymorphicSerializer, \
+                                               jser::SetSerializer,jser::ArraySerializer,jser::ValarraySerializer, \
+                                               jser::ForwardListSerializer,jser::MapSerializer,jser::TupleSerializer, \
+                                               jser::BitsetSerializer,jser::StackAndQueueSerializer,jser::PriorityQueueSerializer,\
+                                               jser::PointerSerializer
 
 #define CREATE_CUSTOM_JSER_MANAGER_TYPE(managerType, ...) using managerType = typename std::variant<__VA_ARGS__>
-#define CREATE_EXTENDED_JSER_MANAGER_TYPE(managerType, ...) using managerType = typename std::variant<DEFAUL_SERIALIZATION_CONSTRUCTOR_HELPER FOR_EACH(COMMACONTENT ,__VA_ARGS__)>
-#define CREATE_DEFAULT_JSER_MANAGER_TYPE(managerType) using managerType = typename std::variant<DEFAUL_SERIALIZATION_CONSTRUCTOR_HELPER>
+#define CREATE_EXTENDED_JSER_MANAGER_TYPE(managerType, ...) using managerType = typename std::variant<JSER_HELPER_DEFAUL_SERIALIZATION_CONSTRUCTOR_HELPER JSER_HELPER_FOR_EACH(JSER_HELPER_COMMACONTENT ,__VA_ARGS__)>
+#define CREATE_DEFAULT_JSER_MANAGER_TYPE(managerType) using managerType = typename std::variant<JSER_HELPER_DEFAUL_SERIALIZATION_CONSTRUCTOR_HELPER>
 
 // Add elements to serialize 
-#define JSER_ADD(managerType, ...) CreateSerializeItem<managerType>(OPEN_BRACKET FOR_EACH(STRWITHCOMMA,__VA_ARGS__) CLOSE_BRACKET FOR_EACH(COMMACONTENT,__VA_ARGS__))
-
-
-
+#define JSER_ADD(managerType, ...) CreateSerializeItem<managerType>(JSER_HELPER_OPEN_BRACKET JSER_HELPER_FOR_EACH(JSER_HELPER_STRWITHCOMMA,__VA_ARGS__) \
+                                    JSER_HELPER_CLOSE_BRACKET JSER_HELPER_FOR_EACH(JSER_HELPER_COMMACONTENT,__VA_ARGS__))
